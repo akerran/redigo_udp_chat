482c973052f0b5018f2866cece72c74859f280f9
Jevgenijs Kaigorodovs <eugene.outbox@inbox.lv>
2021-08-04 00:15:59 +0300

    removed endless history polling, added history send to exact client only

--- server.go
+++ server.go
@@ -6,7 +6,6 @@ import (
     "github.com/gomodule/redigo/redis"
     "sync"
     "strings"
-    "time"
 )
 
 const MAXCHATHISTORY int = 20
@@ -45,10 +44,13 @@ func parse_msg(msg string) (username, msgid string) {
     return username, msgid
 }
 
+
+// search message in hash for current client and remove found message
+// from 'messages' list
 func remove_message(c redis.Conn, user string, msgid string) {
     msg, err := redis.Bytes(c.Do("hget", "user:"+user, msgid))
     if err != nil {
-        fmt.Println("Message not found")
+        fmt.Println("Message not found", err)
     } else {
         c.Do("lrem", "messages", "0", string(msg))
         c.Do("hdel", "user:"+user, msgid)
@@ -67,25 +69,59 @@ func client_listener(wg *sync.WaitGroup, c redis.Conn) {
     }
     defer pc.Close()
 
+
+    // broadcast connection
+    bcastConn,err := net.ListenPacket("udp4", ":8813")
+    if err != nil {
+        panic(err)
+    }
+    defer bcastConn.Close()
+
+    baddr,err := net.ResolveUDPAddr("udp4", "192.168.8.255:8813")
+    if err != nil {
+        panic(err)
+    }
+
     buf := make([]byte, 1024)
+        var history string
     for {
-        n,_,err := pc.ReadFrom(buf)
+        n,clntAddr,err := pc.ReadFrom(buf)
         if err != nil {
             panic(err)
         }
         strmsg := string(buf[:n])
-        fmt.Println("Got message:", strmsg)
-        username,msgid := parse_msg(strmsg)
+        fmt.Println("Got message:", clntAddr, strmsg)
 
+        if strings.Contains(strmsg, "/h")  {
+	    // we need to send history to exact client only
+            // therefore get his IP and append broadcast port
+	    ccaddr := clntAddr.String()
+	    i := strings.Index(ccaddr, ":")
+	    ccaddr = ccaddr[:i] + ":8813"
+	    fmt.Println("client:", ccaddr) 
+            caddr,err := net.ResolveUDPAddr("udp4", ccaddr)
+            if err != nil {
+                panic(err)
+            }
+            history = load_history(c)
+            pc.WriteTo([]byte(history), caddr)
+            continue
+        }
+
+        username,msgid := parse_msg(strmsg)
         if strings.Contains(strmsg, "/rm")  {
             remove_message(c, username, strings.TrimSpace(strmsg[3:]))
         } else {
+            // got standard message
+            // append it to history and send via broadcast to other clients
             db_append(c, username, msgid, strmsg)
+            pc.WriteTo(buf, baddr)
         }
     }
     wg.Done()
 }
 
+// get contents from messages list as a chat history
 func load_history(c redis.Conn) string {
     var s string
     values,err := redis.Values(c.Do("lrange", "messages",0,MAXCHATHISTORY))
@@ -106,27 +142,9 @@ func main() {
     }
     defer redisConn.Close()
 
-    bcastConn,err := net.ListenPacket("udp4", ":8813")
-    if err != nil {
-        panic(err)
-    }
-    defer bcastConn.Close()
-
-    baddr,err := net.ResolveUDPAddr("udp4", "192.168.8.255:8813")
-    if err != nil {
-        panic(err)
-    }
-
     wg := sync.WaitGroup{}
     wg.Add(1)
     go client_listener(&wg, redisConn)
 
-    for {
-        time.Sleep(1/2 * time.Second)
-        bcastConn.WriteTo([]byte(load_history(redisConn)), baddr)
-        if err != nil {
-            panic(err)
-        }
-    }
     wg.Wait()
 }
